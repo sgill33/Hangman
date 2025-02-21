cce61bbe3ee725c9d3d6d731726797afd15745d8
sgill33 <sgill33@calpoly.edu>
2025-02-20 18:37:36 -0800

    feat: added dark theme field to data store

--- app/src/main/java/com/zybooks/hangman/data/DataStore.kt
+++ app/src/main/java/com/zybooks/hangman/data/DataStore.kt
@@ -3,6 +3,7 @@ package com.zybooks.hangman.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -11,7 +12,8 @@ import kotlinx.coroutines.flow.map
 
 data class AppPreferences(
     val gamesWon: Int = 0,
-    val gamesLost: Int = 0
+    val gamesLost: Int = 0,
+    val darkMode: Boolean = false
 )
 
 class AppStorage(private val context: Context){
@@ -23,14 +25,16 @@ class AppStorage(private val context: Context){
     private object PreferenceKeys {
         val GAMES_WON = intPreferencesKey("gamesWon")
         val GAMES_LOST = intPreferencesKey("gamesLost")
+        val DARK_MODE = booleanPreferencesKey("darkMode")
     }
 
     val appPreferencesFlow: Flow<AppPreferences> =
         context.dataStore.data.map { prefs ->
             val gamesWon = prefs[PreferenceKeys.GAMES_WON] ?: 0
             val gamesLost = prefs[PreferenceKeys.GAMES_LOST] ?: 0
+            val darkMode = prefs[PreferenceKeys.DARK_MODE] ?: false
 
-            AppPreferences(gamesWon,gamesLost)
+            AppPreferences(gamesWon,gamesLost, darkMode)
         }
 
     suspend fun incrementGamesWon() {
@@ -44,6 +48,12 @@ class AppStorage(private val context: Context){
         context.dataStore.edit { prefs ->
             val currentWins = prefs[PreferenceKeys.GAMES_LOST] ?: 0
             prefs[PreferenceKeys.GAMES_LOST] = currentWins + 1
+        }
+    }
+
+    suspend fun toggleTheme(isDark: Boolean) {
+        context.dataStore.edit { prefs ->
+            prefs[PreferenceKeys.DARK_MODE] = isDark
         }
     }
 
